0046ad68c08dee0893caec982344c646e4dc2e6e
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-04-22 14:14:51 +0700

    Update domain to plo-curriculum-dsgn

--- PLO_curriculumDsgn1/settings.py
+++ PLO_curriculumDsgn1/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-d@&q_w))4#yjtk&%u_dxdoc-vai(r3w&#4^v7jxtw6xtljf8ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['plocurriculumdsgnbycrw1-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://plocurriculumdsgnbycrw1-production.up.railway.app']
+ALLOWED_HOSTS = ['plo-curriculum-dsgn.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://plo-curriculum-dsgn.up.railway.app/']
 
 
 # Application definition
