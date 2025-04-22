10c6ceff67a7bd8444a6212bf1fbea50e231e840
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-04-22 13:25:34 +0700

    Fix ALLOWED_HOSTS for Railway

--- PLO_curriculumDsgn1/settings.py
+++ PLO_curriculumDsgn1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d@&q_w))4#yjtk&%u_dxdoc-vai(r3w&#4^v7jxtw6xtljf8ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['plocurriculumdsgnbycrw1-production.up.railway.app']
 
 
 # Application definition
