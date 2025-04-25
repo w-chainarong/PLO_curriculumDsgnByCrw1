e3b12829e50c68bf88b9f9cca242cdbd27fecf0e
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-04-26 06:54:29 +0700

    Enable Whitenoise for serving static files in production

--- PLO_curriculumDsgn1/settings.py
+++ PLO_curriculumDsgn1/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # ✅ เพิ่มบรรทัดนี้
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,8 +136,11 @@ STATIC_URL = 'static/'
 
 # ✅ เพิ่มตรงนี้
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
