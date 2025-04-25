342f271f44966bae022e8a77971841a5ff77a03f
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-04-26 06:09:11 +0700

    Fix DisallowedHost: add Railway domain to ALLOWED_HOSTS

--- PLO_curriculumDsgn1/settings.py
+++ PLO_curriculumDsgn1/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-d@&q_w))4#yjtk&%u_dxdoc-vai(r3w&#4^v7jxtw6xtljf8ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    'plo-curriculum-dsgn.up.railway.app',
+    '127.0.0.1',
+    'localhost'
+]
 
 # Application definition
 
