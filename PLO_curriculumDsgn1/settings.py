2fe604b38a395924c7bcb5d6c36b057327450b48
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-04-26 06:41:14 +0700

    Collect static files for production (fix Django admin layout)

--- PLO_curriculumDsgn1/settings.py
+++ PLO_curriculumDsgn1/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# ✅ เพิ่มตรงนี้
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
