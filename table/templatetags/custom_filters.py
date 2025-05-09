ce839b681c9b020323173c495940f0f0effca8f2
w-chainarong <chainarong.w@mail.rmutk.ac.th>
2025-05-09 17:55:32 +0700

    Add custom filter and error page

--- table/templatetags/custom_filters.py
+++ table/templatetags/custom_filters.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+# 🔢 ดึงค่าจาก list ตาม index
 @register.filter
 def index(sequence, position):
     try:
@@ -10,10 +11,12 @@ def index(sequence, position):
     except (IndexError, ValueError, TypeError):
         return ''
 
+# 🔠 แยก string ด้วยตัวแบ่ง เช่น comma
 @register.filter
 def split(value, delimiter=","):
     return value.split(delimiter)
 
+# 🔁 สร้าง range 1 ถึง value
 @register.filter(name='to_range')
 def to_range(value):
     try:
@@ -22,10 +25,12 @@ def to_range(value):
     except (ValueError, TypeError):
         return range(0)
 
+# 🔍 ดึงค่าใน dictionary ตาม key
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, 0)
 
+# ➕ บวกค่าทั้งหมดใน list
 @register.filter(name='sum_list')
 def sum_list(value):
     try:
@@ -33,6 +38,7 @@ def sum_list(value):
     except TypeError:
         return 0
 
+# ➗ แบ่งค่า (ระวังหาร 0)
 @register.filter
 def div(value, arg):
     try:
@@ -40,9 +46,26 @@ def div(value, arg):
     except (ValueError, ZeroDivisionError):
         return 0
 
+# 📌 ดึง attribute ตามชื่อ เช่น course|map:'credits'
 @register.filter
 def map(value, attr_name):
     try:
         return [getattr(obj, attr_name, 0) for obj in value]
     except:
         return []
+
+# 🆕 ดึงค่า K/S/E/C จาก course ตาม type letter
+@register.filter
+def get_ksec_value(course, type_letter):
+    try:
+        if type_letter == 'K':
+            return course.knowledge
+        elif type_letter == 'S':
+            return course.skills
+        elif type_letter == 'E':
+            return course.ethics
+        elif type_letter == 'C':
+            return course.character
+        return ''
+    except:
+        return ''
